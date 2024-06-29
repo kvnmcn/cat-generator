59e41c2af486c9f5e309e67ab63c862be99a4873
kvnmcn <kevinxsch@gmail.com>
2024-06-30 05:00:41 +0800

    fix: button and api logic
    fetch cataas with json
    fix button to work

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const spacebar = document.getElementById('spacebarButton');
-document.addEventListener("keyup", function(event) {
-    if ((event.key === ' ') || event.target === spacebar) {
-        // spacebar.click();
-        fetch('https://cataas.com/cat?type=medium')
-        .then(data => {
-            let imageUrl = data.url;
-            const image = document.getElementById('cat');
-            image.src = imageUrl;
-            console.log("Success");
-            }) 
-            .catch(error => console.error(error));
+document.addEventListener("keyup", function (event) {
+    if ((event.key === ' ')) {
+        fetchCat();
     }
 });
+spacebar.addEventListener('click', function () {
+    fetchCat();
+});
+function fetchCat() {
+    fetch('https://cataas.com/cat?type=medium&json=true')
+        .then(response => response.json())
+        .then(data => renderImage(data))
+        .catch(error => console.error(error));
+}
+function renderImage(data) {
+    {
+        const imageUrl = data._id;
+        const image = document.getElementById('cat');
+        image.src = `https://cataas.com/cat/${imageUrl}`;
+        console.log(imageUrl);
+    }
+}
