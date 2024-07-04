7695d33a6a2fb7e3925b957bbce871e76f2544f4
kvnmcn <kevinxsch@gmail.com>
2024-07-04 22:12:29 +0800

    Revert "feat: add cat fact"
    This reverts commit df403970a4062096de9d644e689140211730fdaf.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const spacebar = document.getElementById('spacebarButton');
 document.addEventListener("keyup", function (event) {
     if ((event.key === ' ')) {
-        fetchCatFact()
         fetchCat();
     }
 });
 spacebar.addEventListener('click', function () {
-    fetchCatFact()
     fetchCat();
 });
 function fetchCat() {
@@ -22,14 +20,4 @@ function renderImage(data) {
         image.src = `https://cataas.com/cat/${imageUrl}`;
         console.log(imageUrl);
     }
-}
-function fetchCatFact() {
-    fetch('https://meowfacts.herokuapp.com/')
-    .then(response => response.json())
-    .then(data => renderCatFact(data))
-    .catch(error => console.error(error));
-}
-function renderCatFact(data) {
-    const fact = document.getElementById('catFact');
-    fact.innerHTML = data.data;
 }
